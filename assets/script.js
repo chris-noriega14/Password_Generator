2c4b851e423221659d81db912fd49357f50a4070
chris-noriega14 <chris.noriega14@hotmail.com>
2021-09-23 23:52:23 -0500

    Updated password generator to include prompts for character length, using uppercase letters, using lowercase letters, using numerical values, and using special characters for password. Generated Password is displayed in prompt

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+// Assignment Code
+var generateBtn = document.getElementById("generate");
+
+// Write password to the #password input
+function writePassword() {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+var pwlength = getlength()
+   
+if (pwlength < 8 || pwlength > 128) {
+  alert("Password length is invalid. Password length should be at least 8 and less than 128 characters.");
+
+writePassword()
+}
+
+var useUpperCase = confirm("Do you want to include uppercase letters?")
+var useLowerCase = confirm("Do you want to include lowercase letters?")
+var useNumeric = confirm("Do you want to include numeric values?")
+var useSpecial = confirm("Do you want to include special characters?")
+
+if (!useUpperCase && !useLowerCase && !useNumeric && !useSpecial) {
+  alert("You must select at least one of the character types.");
+writePassword()
+}
+
+var needsUpperCase = true;
+var needsLowerCase = true;
+var needsNumerical = true;
+var needsSpecial = true;
+
+uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var realPassword = "";
+
+while (realPassword.length < pwlength + 1) {
+  if (needsUpperCase && useUpperCase) {
+    realPassword = realPassword + uppercase[Math.floor(Math.random()*26)]; 
+    needsUpperCase = false; 
+  }
+  else {
+    realPassword = realPassword + lowercase[Math.floor(Math.random()*26)];
+  }
+
+  
+}
+
+alert("Your Generated Password is: " + realPassword);
+
+
+}
+function getlength () {
+  return prompt("What is your desired password length?");
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Set up arrays for password generation criteria
+
+// numeric = [0,1,2,3,4,5,6,7,8,9] //getRandomInt
+// special = [" ","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","\","]","^","_","`","{","|","}","~"]
